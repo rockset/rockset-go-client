fc648d7950a32fb51d4346f4caaa3001ca2f8f9b
Martin Englund <pme@rockset.com>
2021-07-09 17:49:10 +0200

    add missing query lambda methods

--- query_lambdas.go
+++ query_lambdas.go
@@ -11,6 +11,170 @@ import (
 // LatestTag is the query lambda tag for the latest version.
 const LatestTag = "latest"
 
+// CreateQueryLambda creates a new query lambda.
+//
+// https://docs.rockset.com/rest-api/#createquerylambda
+func (rc *RockClient) CreateQueryLambda(ctx context.Context, workspace, name, sql string,
+	options ...option.CreateQueryLambdaOption) (openapi.QueryLambdaVersion, error) {
+	var err error
+	var resp openapi.QueryLambdaVersionResponse
+
+	q := rc.QueryLambdasApi.CreateQueryLambda(ctx, workspace)
+	req := openapi.NewCreateQueryLambdaRequestWithDefaults()
+
+	req.Name = name
+	req.Sql = openapi.QueryLambdaSql{
+		Query: sql,
+	}
+
+	opts := option.CreateQueryLambdaOptions{}
+	for _, o := range options {
+		o(&opts)
+	}
+
+	if opts.Description != nil {
+		req.Description = opts.Description
+	}
+
+	if len(opts.QueryParameters) > 0 {
+		req.Sql.DefaultParameters = &opts.QueryParameters
+	}
+
+	err = rc.Retry(ctx, func() error {
+		resp, _, err = q.Body(*req).Execute()
+		return err
+	})
+
+	if err != nil {
+		return openapi.QueryLambdaVersion{}, err
+	}
+
+	return resp.GetData(), nil
+}
+
+// DeleteQueryLambda deletes a query lambda.
+//
+// https://docs.rockset.com/rest-api/#deletequerylambda
+func (rc *RockClient) DeleteQueryLambda(ctx context.Context, workspace, name string) error {
+	var err error
+
+	q := rc.QueryLambdasApi.DeleteQueryLambda(ctx, workspace, name)
+
+	err = rc.Retry(ctx, func() error {
+		_, _, err = q.Execute()
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateQueryLambda updates an existing query lambda.
+//
+// https://docs.rockset.com/rest-api/#updatequerylambda
+func (rc *RockClient) UpdateQueryLambda(ctx context.Context, workspace, name, sql string,
+	options ...option.CreateQueryLambdaOption) (openapi.QueryLambdaVersion, error) {
+	var err error
+	var resp openapi.QueryLambdaVersionResponse
+
+	q := rc.QueryLambdasApi.UpdateQueryLambda(ctx, workspace, name)
+	req := openapi.NewUpdateQueryLambdaRequestWithDefaults()
+
+	req.Sql = &openapi.QueryLambdaSql{
+		Query: sql,
+	}
+
+	opts := option.CreateQueryLambdaOptions{}
+	for _, o := range options {
+		o(&opts)
+	}
+
+	if opts.Description != nil {
+		req.Description = opts.Description
+	}
+
+	if len(opts.QueryParameters) > 0 {
+		req.Sql.DefaultParameters = &opts.QueryParameters
+	}
+
+	err = rc.Retry(ctx, func() error {
+		resp, _, err = q.Body(*req).Execute()
+		return err
+	})
+
+	if err != nil {
+		return openapi.QueryLambdaVersion{}, err
+	}
+
+	return resp.GetData(), nil
+}
+
+// CreateQueryLambdaTag creates a new tag for the query lambda version.
+//
+// https://docs.rockset.com/rest-api/#createquerylambdatag
+func (rc *RockClient) CreateQueryLambdaTag(ctx context.Context, workspace, name, version, tag string) (openapi.QueryLambdaTag, error) {
+	var err error
+	var resp openapi.QueryLambdaTagResponse
+
+	q := rc.QueryLambdasApi.CreateQueryLambdaTag(ctx, workspace, name)
+	req := openapi.NewCreateQueryLambdaTagRequestWithDefaults()
+
+	req.TagName = tag
+	req.Version = version
+
+	err = rc.Retry(ctx, func() error {
+		resp, _, err = q.Body(*req).Execute()
+		return err
+	})
+
+	if err != nil {
+		return openapi.QueryLambdaTag{}, err
+	}
+
+	return resp.GetData(), nil
+}
+
+// DeleteQueryLambdaTag deletes a query lambda tag.
+//
+// https://docs.rockset.com/rest-api/#deletequerylambdatag
+func (rc *RockClient) DeleteQueryLambdaTag(ctx context.Context, workspace, name, tag string) error {
+	var err error
+
+	q := rc.QueryLambdasApi.DeleteQueryLambdaTag(ctx, workspace, name, tag)
+	err = rc.Retry(ctx, func() error {
+		_, _, err = q.Execute()
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteQueryLambdaVersion deletes a query lambda version.
+//
+// https://docs.rockset.com/rest-api/#deletequerylambdaversion
+func (rc *RockClient) DeleteQueryLambdaVersion(ctx context.Context, workspace, name, version string) error {
+	var err error
+
+	q := rc.QueryLambdasApi.DeleteQueryLambdaVersion(ctx, workspace, name, version)
+	err = rc.Retry(ctx, func() error {
+		_, _, err = q.Execute()
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // ExecuteQueryLambda executes a query lambda with optional query options.
 func (rc *RockClient) ExecuteQueryLambda(ctx context.Context, workspace, name string,
 	options ...option.QueryLambdaOption) (openapi.QueryResponse, error) {
