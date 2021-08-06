db556dd897a7241b4186e3c504932c851f1e2be9
Martin Englund <pme@rockset.com>
2021-08-06 21:42:03 +0200

    replace patch document

--- documents.go
+++ documents.go
@@ -1,7 +1,12 @@
 package rockset
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/rs/zerolog"
 
@@ -39,36 +44,112 @@ func (rc *RockClient) AddDocuments(ctx context.Context, workspace, collection st
 		return nil, err
 	}
 
-	logResult(log.Trace(), resp.GetData()).Msg("added documents")
+	logDocumentStatuses(log.Trace(), resp.GetData()).Msg("added documents")
 
 	return resp.GetData(), nil
+}
+
+type patchDocumentRequest struct {
+	Data []PatchDocument `json:"data"`
+}
+
+type PatchDocument struct {
+	ID      string           `json:"_id"`
+	Patches []PatchOperation `json:"patch"`
+}
+
+type PatchOperation struct {
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value"`
+	From  *string     `json:"from"`
 }
 
 // PatchDocuments updates (patches) existing documents in a collection
 //
 // REST API documentation https://docs.rockset.com/rest-api/#patchdocuments
 func (rc *RockClient) PatchDocuments(ctx context.Context, workspace, collection string,
-	docs []openapi.PatchDocument) ([]openapi.DocumentStatus, error) {
+	patches []PatchDocument) ([]openapi.DocumentStatus, error) {
 	var err error
-	var resp openapi.PatchDocumentsResponse
+	var resp *http.Response
 
-	log := zerolog.Ctx(ctx)
-	q := rc.DocumentsApi.PatchDocuments(ctx, workspace, collection)
-
-	req := openapi.NewPatchDocumentsRequest(docs)
-
-	err = rc.Retry(ctx, func() error {
-		resp, _, err = q.Body(*req).Execute()
-		return err
-	})
-
+	req, err := rc.patchDocumentsRequest(ctx, workspace, collection, patches)
 	if err != nil {
 		return nil, err
 	}
 
-	logResult(log.Trace(), resp.GetData()).Msg("patched documents")
+	err = rc.Retry(ctx, func() error {
+		resp, err = rc.RockConfig.cfg.HTTPClient.Do(req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return resp.GetData(), nil
+	defer closeAndLog(ctx, resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	log := zerolog.Ctx(ctx)
+
+	// should this accept all 2xx status codes?
+	if resp.StatusCode == http.StatusOK {
+		var pdr openapi.PatchDocumentsResponse
+		if err = json.Unmarshal(body, &pdr); err != nil {
+			return nil, err
+		}
+
+		logDocumentStatuses(log.Trace(), pdr.GetData()).Msg("patched documents")
+
+		return pdr.GetData(), nil
+	}
+
+	// if we get here, something went wrong, see if the error can be extracted from the body
+
+	var em openapi.ErrorModel
+	if err = json.Unmarshal(body, &em); err != nil {
+		log.Error().Err(err).Str("body", string(body)).Msg("failed to unmarshal response")
+		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	return nil, Error{
+		ErrorModel: &em,
+		// TODO: set cause too
+	}
+}
+
+func (rc *RockClient) patchDocumentsRequest(ctx context.Context, ws, collection string,
+	patches []PatchDocument) (*http.Request, error) {
+	data := patchDocumentRequest{Data: patches}
+	payload := bytes.Buffer{}
+	e := json.NewEncoder(&payload)
+	if err := e.Encode(data); err != nil {
+		return nil, err
+	}
+
+	// TODO escape workspace and collection
+	u := fmt.Sprintf("%s/v1/orgs/self/ws/%s/collections/%s/docs", rc.RockConfig.APIServer, ws, collection)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPatch, u, &payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "apikey "+rc.RockConfig.APIKey)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("x-rockset-version", Version)
+	req.Header.Set("User-Agent", rc.RockConfig.cfg.UserAgent)
+
+	return req, nil
+}
+
+func closeAndLog(ctx context.Context, c io.Closer) {
+	if err := c.Close(); err != nil {
+		log := zerolog.Ctx(ctx)
+		log.Error().Err(err).Msg("failed to close")
+	}
 }
 
 // DeleteDocuments deletes documents from a collection
@@ -98,12 +179,12 @@ func (rc *RockClient) DeleteDocuments(ctx context.Context, workspace, collection
 		return nil, err
 	}
 
-	logResult(log.Trace(), resp.GetData()).Msg("deleted documents")
+	logDocumentStatuses(log.Trace(), resp.GetData()).Msg("deleted documents")
 
 	return resp.GetData(), nil
 }
 
-func logResult(e *zerolog.Event, statuses []openapi.DocumentStatus) *zerolog.Event {
+func logDocumentStatuses(e *zerolog.Event, statuses []openapi.DocumentStatus) *zerolog.Event {
 	result := map[string]int{}
 	for _, s := range statuses {
 		i := result[s.GetStatus()]
