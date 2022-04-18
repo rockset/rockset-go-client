800fe4bfa292c3b5b1277912fec28255ec27de20
Chris Lowenthal <clo@rockset.com>
2022-04-18 18:46:18 +0000

    fixes generate script, adds config file for openapi generator

--- generate.sh
+++ generate.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-VERSION=$(grep --only-matching --extended-regexp '\d+\.\d+\.\d+' version.go)
+VERSION=$(grep --only-match "[0-9]*\.[0-9]*\.[0-9]*" version.go)
 
 rm -rf openapi
 
 docker run --rm \
     -v "${PWD}:/rockset" openapitools/openapi-generator-cli:v5.4.0 generate \
     -g go \
+    --config /rockset/generator/config.json \
     -i /rockset/spec/spec2.yaml \
     --git-user-id rockset \
     --git-repo-id rockset-go-client \
