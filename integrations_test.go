45ffd14e63b7524bebc158bdf4de0215f15be062
Martin Englund <pme@rockset.com>
2022-10-11 14:09:20 -0700

    turn all integration tests into a test suite

--- integrations_test.go
+++ integrations_test.go
@@ -1,9 +1,7 @@
 package rockset_test
 
 import (
-	"errors"
-	"log"
-	"os"
+	"github.com/stretchr/testify/suite"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,91 +10,67 @@ import (
 	"github.com/rockset/rockset-go-client/option"
 )
 
-func TestRockClient_S3Integration(t *testing.T) {
-	skipUnlessIntegrationTest(t)
-
-	ctx := testCtx()
-	name := "s3test"
-
-	rc, err := rockset.NewClient()
-	require.NoError(t, err)
-
-	// get the integration
-	getReq := rc.IntegrationsApi.GetIntegration(ctx, name)
-	_, _, err = getReq.Execute()
-	if err != nil {
-		// check if it is missing
-		var re rockset.Error
-		if errors.As(err, &re) {
-			if !re.IsNotFoundError() {
-				require.NoError(t, err)
-			}
-		}
-	} else {
-		// the integration exists, delete it
-		deleteReq := rc.IntegrationsApi.DeleteIntegration(ctx, name)
-		_, _, err = deleteReq.Execute()
-		require.NoError(t, err)
-	}
-
-	// create a new integration
-	_, err = rc.CreateS3Integration(ctx, name,
-		option.AWSRole("arn:aws:iam::469279130686:role/rockset-s3-integration"),
-		option.WithS3IntegrationDescription("test"))
-	require.NoError(t, err)
-
-	// list integrations and look for the newly created integration
-	listReq := rc.IntegrationsApi.ListIntegrations(ctx)
-	listResp, _, err := listReq.Execute()
-	require.NoError(t, err)
-
-	var found bool
-	for _, i := range listResp.GetData() {
-		if i.Name == name {
-			found = true
-		}
-	}
-	if !found {
-		t.Errorf("could not find %s", name)
-	}
-
-	// delete
-	deleteReq := rc.IntegrationsApi.DeleteIntegration(ctx, name)
-	_, _, err = deleteReq.Execute()
-	require.NoError(t, err)
+type IntegrationTestSuite struct {
+	suite.Suite
+	rc *rockset.RockClient
 }
 
-func TestRockClient_CreateGCSIntegration(t *testing.T) {
+func TestIntegrationTestSuite(t *testing.T) {
 	skipUnlessIntegrationTest(t)
 
-	saKeyFile := os.Getenv("GCP_SA_KEY_JSON")
-	if saKeyFile == "" {
-		t.Skip("environment variable GCP_SA_KEY_JSON set")
-	}
-
-	ctx := testCtx()
 	rc, err := rockset.NewClient()
 	require.NoError(t, err)
 
-	iName := "testGCSIntegration"
-	gcs, err := rc.GetIntegration(ctx, iName)
-	if err == nil {
-		err = rc.DeleteIntegration(ctx, iName)
-		require.NoError(t, err)
-		log.Printf("collection deleted: %s", iName)
-	} else {
-		var re rockset.Error
-		if !errors.As(err, &re) {
-			t.Fatalf("GetIntegration failed for %s: %v", iName, err)
-		}
-		if !re.IsNotFoundError() {
-			t.Fatalf("GetIntegration failed for %s: %v", iName, re)
-		}
-	}
+	suite.Run(t, &IntegrationTestSuite{rc: rc})
+}
 
-	gcs, err = rc.CreateGCSIntegration(ctx, iName, saKeyFile,
+func (s *IntegrationTestSuite) TearDown() {
+	ctx := testCtx()
+
+	// clean up any lingering integrations
+	_ = s.rc.DeleteIntegration(ctx, "s3test")
+	_ = s.rc.DeleteIntegration(ctx, "testGCSIntegration")
+}
+
+func (s *IntegrationTestSuite) TestGetIntegration() {
+	ctx := testCtx()
+
+	const iName = "confluent-cloud"
+	integration, err := s.rc.GetIntegration(ctx, iName)
+	s.NoError(err)
+	s.Assert().Equal(iName, integration.GetName())
+}
+
+func (s *IntegrationTestSuite) TestListIntegrations() {
+	ctx := testCtx()
+
+	_, err := s.rc.ListIntegrations(ctx)
+	s.NoError(err)
+}
+
+func (s *IntegrationTestSuite) TestCreateS3Integration() {
+	ctx := testCtx()
+	const name = "s3test"
+
+	_, err := s.rc.CreateS3Integration(ctx, name,
+		option.AWSRole("arn:aws:iam::469279130686:role/rockset-s3-integration"),
+		option.WithS3IntegrationDescription("test"))
+	s.Require().NoError(err)
+
+	err = s.rc.DeleteIntegration(ctx, name)
+	s.Require().NoError(err)
+}
+
+func (s *IntegrationTestSuite) TestCreateGCSIntegration() {
+	saKeyFile := skipUnlessEnvSet(s.T(), "GCP_SA_KEY_JSON")
+
+	ctx := testCtx()
+	name := "testGCSIntegration"
+
+	_, err := s.rc.CreateGCSIntegration(ctx, name, saKeyFile,
 		option.WithGCSIntegrationDescription("created by rockset integration tests"))
-	require.NoError(t, err)
+	s.Require().NoError(err)
 
-	log.Printf("created gcs integration: %s", gcs.GetName())
+	err = s.rc.DeleteIntegration(ctx, name)
+	s.Require().NoError(err)
 }
