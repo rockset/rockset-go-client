9df63c1724e00b9cbc6858b0cbc19f55e3bc6eb6
Martin Englund <pme@rockset.com>
2023-10-02 13:50:53 -0700

    add option to enable/disable remount on resume

--- virtual_instances_test.go
+++ virtual_instances_test.go
@@ -126,6 +126,7 @@ func (s *VirtualInstanceIntegrationSuite) TestVirtualInstance_0_Create() {
 	vi, err := rc.CreateVirtualInstance(ctx, s.name,
 		option.WithVirtualInstanceSize(option.SizeSmall),
 		option.WithNoMountRefresh(),
+		option.WithRemountOnResume(true),
 		option.WithAutoSuspend(15*time.Minute),
 	)
 	s.Require().NoError(err)
