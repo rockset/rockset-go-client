5f3af4cb42b473eaa299937add62d41ac0652353
Steven Baldwin <87034744+sbaldwin-rs@users.noreply.github.com>
2024-01-08 10:17:10 -0800

    Use git-lfs for cassettes (#198)
    * Use git-lfs for cassettes
    
    * Add cassettes
    
    * Remove cassette versioning
    
    * Update test asset path

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/rockset/rockset-go-client
 go 1.21
 
 require (
-	github.com/aws/aws-sdk-go-v2/config v1.24.0
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.42.1
 	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/fatih/structs v1.1.0
 	github.com/go-zookeeper/zk v1.0.3
@@ -21,20 +19,16 @@ require (
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.22.2 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.0 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.15.2 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.3 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.24.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.11.74 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.2 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.2.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.2.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.16.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.17.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.19.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.25.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.42.1 // indirect
 	github.com/aws/smithy-go v1.16.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/containerd/continuity v0.3.0 // indirect
--- vcr_test.go
+++ vcr_test.go
@@ -1,17 +1,13 @@
 package rockset_test
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"strings"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/seborama/govcr/v14"
 	"github.com/seborama/govcr/v14/cassette/track"
-	"github.com/seborama/govcr/v14/fileio"
 	"github.com/stretchr/testify/require"
 
 	"github.com/rockset/rockset-go-client"
@@ -27,12 +23,6 @@ func vcrTestClient(t *testing.T, name string) (*rockset.RockClient, func(string)
 	return rc, fn
 }
 
-const vcrBucket = "rockset-go-tests"
-
-func vcrBucketPath(name string) string {
-	return fmt.Sprintf("/%s/vcr/%s/%s.cassette.gz", vcrBucket, rockset.Version, name)
-}
-
 func vcrClient(name string) (*rockset.RockClient, func(string) string, error) {
 	return vcrClientWrapper(strings.ToLower(os.Getenv("VCR_MODE")), name)
 }
@@ -44,14 +34,7 @@ func vcrClientWrapper(mode, name string) (*rockset.RockClient, func(string) stri
 		rockset.WithCustomHeader("x-rockset-test", "go-client"),
 	}
 	var settings []govcr.Setting
-	path := vcrBucketPath(name)
-
-	// VCR cassettes are in S3 bucket rockset-go-tests (in us-west-2)
-	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion("us-west-2"))
-	if err != nil {
-		return nil, nil, err
-	}
-	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) { o.UsePathStyle = true })
+	path := fmt.Sprintf("./testdata/cassettes/%s.cassette.gz", name)
 
 	switch mode {
 	case "record":
@@ -75,22 +58,12 @@ func vcrClientWrapper(mode, name string) (*rockset.RockClient, func(string) stri
 		return nil, nil, fmt.Errorf("unknown VCR_MODE: %s", mode)
 	}
 
-	vcr := govcr.NewVCR(govcr.NewCassetteLoader(path).WithStore(fileio.NewAWS(s3Client)), settings...)
+	vcr := govcr.NewVCR(govcr.NewCassetteLoader(path), settings...)
 	options = append(options, rockset.WithHTTPClient(vcr.HTTPClient()),
 		rockset.WithCustomHeader("x-rockset-test", "go-client"))
 
 	rc, err := rockset.NewClient(options...)
 	return rc, randFn, err
-}
-
-// stripPatchVersion removes the patch version from a SemVer string
-func stripPatchVersion(v string) string {
-	fields := strings.Split(v, ".")
-	if len(fields) != 3 {
-		panic("malformed version string: " + v)
-	}
-
-	return strings.Join(fields[:2], ".")
 }
 
 // VCR settings that exclude the HTTP header Authorization and ignores the patch version
@@ -101,15 +74,6 @@ func vcrSettings(offline bool) []govcr.Setting {
 			func(httpRequest, trackRequest *track.Request) bool {
 				httpRequest.Header.Del(authHeader)
 				trackRequest.Header.Del(authHeader)
-
-				v := httpRequest.Header.Get(rockset.HeaderVersionName)
-				v = stripPatchVersion(v)
-				httpRequest.Header.Set(rockset.HeaderVersionName, v)
-
-				v = trackRequest.Header.Get(rockset.HeaderVersionName)
-				v = stripPatchVersion(v)
-				trackRequest.Header.Set(rockset.HeaderVersionName, v)
-
 				return govcr.DefaultHeaderMatcher(httpRequest, trackRequest)
 			},
 		),
