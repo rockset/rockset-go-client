b43e5ac399012b06119a4893cacb6e453b4c452c
Martin Englund <pme@rockset.com>
2024-01-12 13:49:10 -0800

    version 0.22.6

--- openapi/configuration.go
+++ openapi/configuration.go
@@ -86,7 +86,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/0.22.5/go",
+		UserAgent:     "OpenAPI-Generator/0.22.6/go",
 		Debug:         false,
 		Servers: ServerConfigurations{
 			{
