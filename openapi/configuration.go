47f06a727b8d6d45c75f082430ff307b651d9a77
Kristie Lim <kristie@rockset.com>
2024-03-06 14:19:28 -0800

    v0.24.1

--- openapi/configuration.go
+++ openapi/configuration.go
@@ -86,7 +86,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.24.0/go",
+		UserAgent:        "OpenAPI-Generator/0.24.1/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package rockset
 
 // Version is the Rockset client version
-const Version = "0.24.0"
+const Version = "0.24.1"
